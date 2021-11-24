932d9645bb7bb1309644bcf9c4aa1faea8e55015
raihanr1 <raihanrobbani123@gmail.com>
2021-11-24 08:55:52 +0700

    add option list navbar component for home page

---
+++ client-admin/src/components/OptionList.js
@@ -0,0 +1,18 @@
+export default function OptionList() {
+  return (
+    <>
+      <li className="sidebar-item active">
+        <a className="sidebar-link">
+          <i className="bi bi-grid-fill"></i>
+          <span>Register Admin</span>
+        </a>
+      </li>
+      <li className="sidebar-item active">
+        <a className="sidebar-link">
+          <i className="bi bi-grid-fill"></i>
+          <span>Create New Product</span>
+        </a>
+      </li>
+    </>
+  );
+}
