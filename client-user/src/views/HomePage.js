578ec11e37f97405726c24b332c68799668ce12a
raihanr1 <raihanrobbani123@gmail.com>
2021-11-28 17:18:18 +0700

    add element jsx homepage client-user

--- client-user/src/views/HomePage.js
+++ client-user/src/views/HomePage.js
@@ -1,98 +1,57 @@
-import CardCategory from "../components/Card/CardProduct";
-
+import CardCategory from "../components/Card/CardCategory";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import CardProduct from "../components/Card-Product/CardProduct";
+import { fetchAllProduct } from "../store/root-reducer/action/actionProduct";
+import { useState } from "react";
 export default function HomePage() {
+  const dispatch = useDispatch();
+  const { product, category, productLoading, errorProduct, filter } =
+    useSelector((state) => state);
+  let productFilter = [];
+  let theme = "This Is Our Product";
+  if (filter !== "all" && filter !== null && product.length) {
+    console.log(product, "ada emng???");
+    theme = `This Is Our Product of ${filter.name}`;
+    productFilter = product.filter((el) => el.CategoryId == filter.id);
+  } else if (product.length) {
+    productFilter = product;
+  }
+  useEffect(() => {
+    dispatch(fetchAllProduct());
+  }, [filter]);
   return (
     <>
-      <CardCategory />
-      <div className="container">
-        <div className="carousel slide" id="main-carousel" data-ride="carousel">
-          <ol className="carousel-indicators">
-            <li
-              data-target="#main-carousel"
-              data-slide-to="0"
-              className="active"
-            ></li>
-            <li data-target="#main-carousel" data-slide-to="1"></li>
-            <li data-target="#main-carousel" data-slide-to="2"></li>
-            <li data-target="#main-carousel" data-slide-to="3"></li>
-          </ol>
-
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img
-                className="d-block img-fluid"
-                src="https://s19.postimg.cc/qzj5uncgj/slide1.jpg"
-                alt=""
-              ></img>
-              <div className="carousel-caption d-none d-md-block">
-                <h1>lala</h1>
-              </div>
-            </div>
-
-            <div className="carousel-item">
-              <img
-                className="d-block img-fluid"
-                src="https://s19.postimg.cc/lmubh3h0j/slide2.jpg"
-                alt=""
-              ></img>
-              <div className="carousel-caption d-none d-md-block">
-                <h3>dsadsad</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Adipisci esse vitae exercitationem fugit, numquam minus!
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                className="d-block img-fluid"
-                src="https://s19.postimg.cc/99hh9lr5v/slide3.jpg"
-                alt=""
-              ></img>
-              <div className="carousel-caption d-none d-md-block">
-                <h3>lalaa</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Adipisci esse vitae exercitationem fugit, numquam minus!
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://s19.postimg.cc/nenabzsnn/slide4.jpg"
-                alt=""
-                className="d-block img-fluid"
-              ></img>
-              <div className="carousel-caption d-none d-md-block">
-                <h3>baba</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Adipisci esse vitae exercitationem fugit, numquam minus!
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <a
-            href="#main-carousel"
-            className="carousel-control-prev"
-            data-slide="prev"
-          >
-            <span className="carousel-control-prev-icon"></span>
-            <span className="sr-only" aria-hidden="true">
-              Prev
-            </span>
-          </a>
-          <a
-            href="#main-carousel"
-            className="carousel-control-next"
-            data-slide="next"
-          >
-            <span className="carousel-control-next-icon"></span>
-            <span className="sr-only" aria-hidden="true">
-              Next
-            </span>
-          </a>
+      <div
+        style={{
+          maxWidth: "1200px",
+          display: "flex",
+          marginTop: "30px",
+        }}
+      >
+        {category.map((el, i) => {
+          if (i < 3) {
+            return <CardCategory key={el.id} category={el} />;
+          }
+        })}
+      </div>
+      <div className="container" style={{ marginTop: "80px" }}>
+        <p
+          style={{
+            textAlign: "center",
+            marginBottom: "50px",
+            fontFamily: "fantasy",
+            fontSize: "50px",
+          }}
+        >
+          {theme}
+        </p>
+        <div className="row">
+          {productFilter.length
+            ? productFilter.map((el, i) => {
+                return <CardProduct key={el.id} product={el} />;
+              })
+            : null}
         </div>
       </div>
     </>
