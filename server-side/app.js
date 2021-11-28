7b5bf74ea67f635d61ec0a2e866dc0a6dba6087f
raihanr1 <raihanrobbani123@gmail.com>
2021-11-28 17:39:09 +0700

    ready to deploy

--- server-side/app.js
+++ server-side/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const express = require("express");
 const router = require("./routes/index.js");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- server-side/seeders/20211128082927-seedUsersTable.js
+++ server-side/seeders/20211128082927-seedUsersTable.js
@@ -1,24 +1,27 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          username: "raihan",
+          email: "raihanrobbani123@gmail.com",
+          password:
+            "$2a$10$zVxUyFlee18P6Qyab/efL.ZrnpgF6YXpvyleY5UIC8I0U8ay7UmXW",
+          role: "Admin",
+          phoneNumber: "085156054979",
+          address: "Bogor",
+          createdAt: "2021-11-26T03:32:53.905Z",
+          updatedAt: "2021-11-26T03:32:53.905Z",
+        },
+      ],
+      {}
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    await queryInterface.bulkDelete("Users", null, {});
+  },
 };
